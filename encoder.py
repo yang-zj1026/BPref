ab7209c96a5d6ad603ad02a4f362a4d62e31fe91
yang-zj1026 <victoryang1026@gmail.com>
2023-09-24 03:27:03 -0700

    Add encoder

---
+++ encoder.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+
+
+def tie_weights(src, trg):
+    assert type(src) == type(trg)
+    trg.weight = src.weight
+    trg.bias = src.bias
+
+
+OUT_DIM = {2: 39, 4: 35, 6: 31}
+OUT_DIM_64 = {2: 29, 4: 25, 6: 21}
+OUT_DIM_108 = {4: 47}
+
+
+class PixelEncoder(nn.Module):
+    """Convolutional encoder of pixels observations."""
+
+    def __init__(self, obs_shape, feature_dim, num_layers=2, num_filters=32, output_logits=False, num_envs=1,
+                 add_norm=None):
+        super().__init__()
+
+        assert len(obs_shape) == 3
+        self.obs_shape = obs_shape
+        self.feature_dim = feature_dim
+        self.num_layers = num_layers
+        self.num_envs = num_envs
+        self.add_norm = add_norm
+        # try 2 5x5s with strides 2x2. with samep adding, it should reduce 84 to 21, so with valid, it should be even smaller than 21.
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(obs_shape[0], num_filters, 3, stride=2)]
+        )
+        for i in range(num_layers - 1):
+            self.convs.append(nn.Conv2d(num_filters, num_filters, 3, stride=1))
+
+        self.norm_layers = None
+        if self.add_norm is not None:
+            if self.add_norm == 'group_norm':
+                self.norm_layers = nn.ModuleList([nn.GroupNorm(8, num_filters) for i in range(num_layers)])
+            elif self.add_norm == 'batch_norm':
+                self.norm_layers = nn.ModuleList([nn.BatchNorm2d(num_filters) for i in range(num_layers)])
+            else:
+                raise NotImplementedError
+
+        if obs_shape[-1] == 108:
+            assert num_layers in OUT_DIM_108
+            out_dim = OUT_DIM_108[num_layers]
+        elif obs_shape[-1] == 64:
+            out_dim = OUT_DIM_64[num_layers]
+        else:
+            out_dim = OUT_DIM[num_layers]
+
+        self.fc = nn.Linear(num_filters * out_dim * out_dim, self.feature_dim)
+        self.ln = nn.LayerNorm(self.feature_dim)
+
+        self.outputs = dict()
+        self.output_logits = output_logits
+
+    def reparameterize(self, mu, logstd):
+        std = torch.exp(logstd)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward_conv(self, obs):
+        if obs.max() > 1.:
+            obs = obs / 255.
+
+        self.outputs['obs'] = obs
+
+        conv = torch.relu(self.convs[0](obs))
+        self.outputs['conv1'] = conv
+
+        for i in range(1, self.num_layers):
+            conv = torch.relu(self.convs[i](conv))
+            if self.norm_layers is not None:
+                conv = self.norm_layers[i](conv)
+            self.outputs['conv%s' % (i + 1)] = conv
+
+        h = conv.view(conv.size(0), -1)
+        return h
+
+    def forward(self, obs, detach=False):
+        # obs shape: [bs, 3, 84, 84]
+        h = self.forward_conv(obs)  # h shape: [bs, out_dim*out_dim]
+
+        if detach:
+            h = h.detach()
+
+        h_fc = self.fc(h)  # h_fc shape: [bs, feature_dim]
+        self.outputs['fc'] = h_fc
+
+        h_norm = self.ln(h_fc)  # h_norm shape: [bs, feature_dim]
+        self.outputs['ln'] = h_norm
+
+        if self.output_logits:
+            out = h_norm
+        else:
+            out = torch.tanh(h_norm)
+            self.outputs['tanh'] = out
+
+        return out  # out shape: [bs, feature_dim]
+
+    def copy_conv_weights_from(self, source):
+        """Tie convolutional layers"""
+        # only tie conv layers
+        for i in range(self.num_layers):
+            tie_weights(src=source.convs[i], trg=self.convs[i])
+
+    def log(self, L, step, log_freq):
+        if step % log_freq != 0:
+            return
+
+        for k, v in self.outputs.items():
+            L.log_histogram('train_encoder/%s_hist' % k, v, step)
+            if len(v.shape) > 2:
+                L.log_image('train_encoder/%s_img' % k, v[0], step)
+
+        for i in range(self.num_layers):
+            L.log_param('train_encoder/conv%s' % (i + 1), self.convs[i], step)
+        L.log_param('train_encoder/fc', self.fc, step)
+        L.log_param('train_encoder/ln', self.ln, step)
+
+
+class IdentityEncoder(nn.Module):
+    def __init__(self, obs_shape, feature_dim, num_layers, num_filters, *args):
+        super().__init__()
+
+        assert len(obs_shape) == 1
+        self.feature_dim = obs_shape[0]
+
+    def forward(self, obs, detach=False):
+        return obs
+
+    def copy_conv_weights_from(self, source):
+        pass
+
+    def log(self, L, step, log_freq):
+        pass
+
+
+_AVAILABLE_ENCODERS = {'pixel': PixelEncoder, 'identity': IdentityEncoder}
+
+
+def make_encoder(
+        encoder_type, obs_shape, feature_dim, num_layers, num_filters, output_logits=False, num_envs=1, add_norm=None
+):
+    assert encoder_type in _AVAILABLE_ENCODERS
+    return _AVAILABLE_ENCODERS[encoder_type](
+        obs_shape, feature_dim, num_layers, num_filters, output_logits, num_envs=num_envs, add_norm=add_norm
+    )
